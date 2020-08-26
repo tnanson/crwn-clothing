c5284790455d899d8dd65fe420b9a84683489161
unknown <nansont@yahoo.com>
2020-08-26 09:22:14 -0400

    update withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Route,Switch, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+function Home(props) {
+  console.log('Home',props);
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+}
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={HomePage}></Route>
+      <Route path='/shop/hats' component={Home}></Route>  
     </div>
   );
 }
