9a6c3c0e1756697c22e3a8be2c3db2e8428449b8
unknown <nansont@yahoo.com>
2020-09-05 12:54:03 -0400

    Adding Directory reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
